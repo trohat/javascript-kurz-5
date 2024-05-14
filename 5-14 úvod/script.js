82db4d49c834630abe092630b531e289fcf46280
Trohat <petr.sed@centrum.cz>
2024-05-14 19:38:56 +0200

    JS - úvod

---
+++ 5-14 úvod/script.js
@@ -0,0 +1,36 @@
+console.log("Učíme se JavaScript! Hurá!");
+console.log(8 + 7);
+console.log("Ahoj");
+
+let vyska;  // založení proměnné
+vyska = 10; // přiřazovací příkaz
+// KAM = CO
+vyska = 50 + 6 * 3;
+
+/* PŘESTÁVKA DO 19.40 - takhle vypadá víceřádkový komentář
+a snězte si svačinu
+a zajděte si na záchod */
+
+let cislo = 25;
+// tohle je věk mého staršího dítěte
+// TAKHLE NE
+
+// alert("Ahoj, jak se máš? Přeji ti krásné narozeniny!");
+
+let vek = 25;
+let jmeno = "Petr";
+// alert("Ahoj já jsem " + jmeno + " a je mi " + vek + " let");
+
+let uzivateluvVek = Number(prompt('Přiznej se, kolik je ti "let"?????'));    
+
+console.log("Uživatel zadal: " + uzivateluvVek);
+
+let uzivatelZa10Let = uzivateluvVek + 10;
+
+let uzivatelZa20Let = uzivateluvVek + 20;
+let uzivatelZa20LetVypocitanoJinak = uzivatelZa10Let + 10;
+
+console.log("Za deset let ti bude " + uzivatelZa10Let);
+console.log("Za dvacet let ti bude " + uzivatelZa20Let)
+
+
